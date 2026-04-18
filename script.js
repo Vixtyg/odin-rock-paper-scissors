6c26582e9857a2ba00eb91f112cae5f2f32aa7ca
Timur Alimov <timuralimov181@gmail.com>
2026-04-19 01:24:49 +0200

    Added Number generation - bug: Its biased (choosing 2 too much, 14 is crit. value, we get 22)

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("hello world");
+function getComputerChoice(){
+    let randomInt=((Math.random())+(Math.random()));
+    randomInt=Math.round(randomInt)
+    console.log(randomInt);
+    switch(randomInt){
+        case 1:
+            return "Rock";
+        case 2:
+            return "Paper";
+        case 3:
+            return "Scissors";
+    }
+}  
+function getHumanChoice(){
+    return humanChoice=prompt("Rock, Paper, Or Scissors?").toLowerCase();
+}
+console.log(getComputerChoice());
+
+console.log(getComputerChoice());
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+
+console.log(getComputerChoice());
